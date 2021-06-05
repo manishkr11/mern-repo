29528812b61ee8fef010ecb38e74c39ead7d05a1
manish kumar <aimaanish1503@gmail.com>
2021-06-05 13:32:46 +0530

    server implemented

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://manish123:manish123@cluster0.9ohyw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+//Bodyparser Middleware
+
+app.use(bodyParser.json())
+
+// DB Config
+
+const db = require('./config/keys').mongoURI
+
+//Connect to Mongo
+
+mongoose.connect(db)
+.then(()=> console.log('MongoDB Connnected'))
+.catch(err=> console.log(err))
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => console.log(`server started on port ${port}`))
