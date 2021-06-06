ef24ca4d0c7e81cc5fb6022c212d54363e8b80fd
manish kumar <aimaanish1503@gmail.com>
2021-06-06 17:58:30 +0530

    fixed css import

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { Button } from 'reactstrap';
 
 function App() {
   return (
     <div className="App">
       <h2>Hello!!</h2>
+      <Button color="primary">primary</Button>{' '}
     </div>
   );
 }
